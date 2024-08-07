f12b9d5a2acdc5ff28cbf0fba9a042807499c405
d3j1x <d3j1x@yahoo.com>
2024-08-07 18:47:23 +0100

    start

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "../app.css";
+    import islogo from "$lib/img/sss.png";
+</script>
+
+<img src={islogo} class=" min-w-full max-h-60 rounded" alt="Flowbite Logo" />
+
+
+
+<slot></slot>
