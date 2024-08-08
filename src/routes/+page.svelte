6b1130a0d65ce08fb0834ad294f77b0868055235
d3j1x <d3j1x@yahoo.com>
2024-08-08 15:00:37 +0100

    update page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,16 @@
 <script>
     import bannerlogo from "$lib/img/banner.png";
+    
+    import { Gallery } from 'flowbite-svelte';
+  
+    const images = [
+      { alt: 'shoes', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg' },
+      { alt: 'small bag', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg' },
+      { alt: 'plants', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg' },
+      { alt: 'watch', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg' }
+    ];
 </script>
+
 <img src={bannerlogo} class="border shadow-2xl my-2 border-black flex justify-center items-center mx-auto max-h-60" alt="Imeen Store Logo" />
+
+<Gallery class="gap-2 grid-cols-2" items={images} />
