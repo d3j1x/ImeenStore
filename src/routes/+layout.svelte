1529267871c8999b8a869b792d27d8146cf404a0
d3j1x <d3j1x@yahoo.com>
2024-08-11 21:04:40 +0100

    add loader

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,32 @@
 <script>
     import "../app.css";
     import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+
+    let x =true;
+    const ToOther = () => {
+      setTimeout(() => {
+        x = false; 
+      }, 2222); 
+    };
+
+  ToOther();
+
+
 </script>
 
 
+
+{#if x}
+    <div class="h-screen  text-center justify-center items-center self-center content-center mx-20">
+      <h1 style="font-family: 'Reem Kufi Fun', sans-serif;" 
+          class="animate-bounce rounded text-7xl shadow-2xl border-2 border-black p-10">
+          <span class="">متجر إيـمـان</span>
+        </h1>
+
+    </div>
+
+
+{:else}
 
 <Navbar style="font-family: 'Reem Kufi Fun', sans-serif;" class="my-2" >
     <NavBrand href="/">
@@ -14,12 +37,21 @@
       <NavLi class="md:text-xl" href="/">الرئيسية</NavLi>
       <NavLi class="md:text-xl" href="/">لباس شرعي</NavLi>
       <NavLi class="md:text-xl" href="/">مكملات</NavLi>
-      <NavLi class="md:text-xl" href="/">اتصال</NavLi>
+      <NavLi class="md:text-xl" href="/contact">اتصال</NavLi>
     </NavUl>
 </Navbar>
 
-
-
-
 <slot></slot>
+  
+{/if}
+
+
+
+
+
+
+
+
+
+
 
